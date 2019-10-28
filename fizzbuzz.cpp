87e29c1e44ae97853fc95f1d094eef0ac92a26f0
Grant Montgomery <montg433@d.umn.edu>
2019-10-28 17:36:26 -0500

    initial commit

---
+++ fizzbuzz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin:
+using std::endl;
+using std::vector;
+
+int main() {
+  vector<string> words {"fizz", "buzz"};
+  vector<int> numbers = {3, 5}
+  int counter;
+  string temp;
+
+}
