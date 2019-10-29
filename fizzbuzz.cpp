c0464210eeb8faf9e058744f2a1399a9477c091d
Grant Montgomery <montg433@d.umn.edu>
2019-10-29 13:08:29 -0500

    added loop to multiply lcm of all numbers in the numbers vector, to determine length of repetition

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -8,26 +8,33 @@ using std::string;
 using std::to_string;
 
 int main() {
-  vector<string> words {"fizz", "buzz"}; //words to subsitute for multiples
-  vector<int> numbers = {3, 5}; //multiples to substitute
+  vector<string> words {"fizz", "buzz"}; //vector of strings
+  vector<int> numbers = {3, 5}; //vector of numbers, should all be prime
   int counter = 1;
+  int range = 1;
   string output = "";
-  string input;
 
-  while(true) {
+  //Calculate lcm of numbers in numbers
+  for(int x : numbers) {
+    range *= x;
+  }
+
+  //Main loop
+  for(counter = 1; counter <= range; counter++) {
+    //Check multiples, and concat output with appropriate strings
     for(int i = 0; i < numbers.size(); i++) {
       if(counter % numbers[i] == 0) {
         output += words[i];
       }
     }
 
+    //Concat the number if it is not a multiple of any numbers in numbers
     if(output == "") {
       output += to_string(counter);
     }
 
-    // cin.get();
+    //Output result
     cout << output << endl;
-    counter++;
     output = "";
   }
 }
