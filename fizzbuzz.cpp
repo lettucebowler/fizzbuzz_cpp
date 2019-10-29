7d869311e9cb68923ea74707fb42aed2ad4055fe
Grant Montgomery <montg433@d.umn.edu>
2019-10-29 12:56:19 -0500

    loops forever until stopped

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include <vector>
 using std::cout;
-using std::cin:
+using std::cin;
 using std::endl;
 using std::vector;
+using std::string;
+using std::to_string;
 
 int main() {
-  vector<string> words {"fizz", "buzz"};
-  vector<int> numbers = {3, 5}
-  int counter;
-  string temp;
+  vector<string> words {"fizz", "buzz"}; //words to subsitute for multiples
+  vector<int> numbers = {3, 5}; //multiples to substitute
+  int counter = 1;
+  string output = "";
+  string input;
 
+  while(true) {
+    for(int i = 0; i < numbers.size(); i++) {
+      if(counter % numbers[i] == 0) {
+        output += words[i];
+      }
+    }
+
+    if(output == "") {
+      output += to_string(counter);
+    }
+
+    // cin.get();
+    cout << output << endl;
+    counter++;
+    output = "";
+  }
 }
