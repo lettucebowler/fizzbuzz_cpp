aa5864bca44123b0b9d93df2e46ad8b0419cdae3
Grant Montgomery <montg433@d.umn.edu>
2020-03-22 15:03:42 -0500

    it's better now

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -18,6 +18,20 @@ ll findlcm(std::vector<int> arr, int n) {
     return ans; 
 } 
 
+std::string prepOutput(std::vector<int> numbers, std::vector<std::string> words, int n) {
+  std::string output = "";
+  for(int i = 0; i < numbers.size(); i++) {
+      if(n % numbers[i] == 0) {
+        output += words[i];
+      }
+    }
+    //Concat the number if it is not a multiple of any numbers in numbers
+    if(output == "") {
+      output += std::to_string(n);
+    }
+    return output;
+}
+
 int main() {
   std::vector<std::string> words {"fizz", "buzz"}; //vector of strings
   std::vector<int> numbers = {3, 5}; //vector of numbers, should all be prime
@@ -27,20 +41,6 @@ int main() {
 
   //Main loop
   for(counter = 1; counter <= range; counter++) {
-    //Check multiples, and concat output with appropriate strings
-    for(int i = 0; i < numbers.size(); i++) {
-      if(counter % numbers[i] == 0) {
-        output += words[i];
-      }
-    }
-
-    //Concat the number if it is not a multiple of any numbers in numbers
-    if(output == "") {
-      output += std::to_string(counter);
-    }
-
-    //Output result
-    std::cout << output << std::endl;
-    output = "";
+    std::cout << prepOutput(numbers, words, counter) << std::endl;
   }
 }
