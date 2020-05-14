bceefdff8478291147a3c4084c1271cb826aac05
Grant Montgomery <lettucebowler@gmail.com>
2020-05-13 20:54:15 -0500

    renamed functions

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -10,7 +10,7 @@ int gcd(int a, int b) {
     return gcd(b, a % b); 
 } 
 
-ll findlcm(std::vector<int> arr, int n) { 
+ll find_lcm(std::vector<int> arr, int n) { 
     ll ans = arr[0]; 
     for (int i = 1; i < n; i++) 
         ans = (((arr[i] * ans)) / 
@@ -18,7 +18,7 @@ ll findlcm(std::vector<int> arr, int n) {
     return ans; 
 } 
 
-std::string prepOutput(std::vector<int> numbers, std::vector<std::string> words, int n) {
+std::string prep_output(std::vector<int> numbers, std::vector<std::string> words, int n) {
   std::string output = "";
   for(int i = 0; i < numbers.size(); i++) {
       if(n % numbers[i] == 0) {
@@ -32,13 +32,13 @@ std::string prepOutput(std::vector<int> numbers, std::vector<std::string> words,
 }
 
 int main() {
-  std::vector<std::string> words {"fizz", "buzz"};
-  std::vector<int> numbers = {3, 5}; //Works best if all are prime, don't have to be though
-  int range = findlcm(numbers, numbers.size());
+  std::vector<std::string> words {"fizz", "buzz", "fuzz"};
+  std::vector<int> numbers = {3, 5, 7}; //Works best if all are prime, don't have to be though
+  int range = find_lcm(numbers, numbers.size());
 
   if (words.size() == numbers.size()) {
     for(int counter = 1; counter <= range; counter++) {
-      std::cout << prepOutput(numbers, words, counter) << std::endl;
+      std::cout << prep_output(numbers, words, counter) << std::endl;
     }
   } else {
     std::cout << "wordlist and numList not equal length" << std::endl;
